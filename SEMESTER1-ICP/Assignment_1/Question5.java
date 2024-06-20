e75be8b16216d806268a742856ae715e12c2f2ce
Pratehu <pragyanpriyadarshini107@gmail.com>
2024-06-20 18:59:55 +0530

    Update

---
+++ SEMESTER1-ICP/Assignment_1/Question5.java
@@ -0,0 +1,12 @@
+
+public class Question5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String m="Hey Dev ";
+		String n="Good morning!";
+		System.out.print(m);
+		System.out.print(n);
+	}
+
+}
---
+++ SEMESTER1-ICP/Assignment_1/Question7.java
@@ -0,0 +1,16 @@
+
+public class Question7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=10;
+		int y=20;
+		int z;
+		z=x;
+		x=y;
+		y=z;
+		System.out.println("x="+x);
+		System.out.println("y="+y);
+	}
+
+}
