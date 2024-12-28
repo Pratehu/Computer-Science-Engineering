18fb05f27b97c3323c544da041c0a2fdfa110fc8
Pratehu <pragyanpriyadarshini107@gmail.com>
2024-12-28 23:38:26 +0530

    UPDATED

---
+++ SEMESTER5/PPWC/Assignment5/Question16.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){int b=65;
+void p=b;
+printf("%d",p);return 0;}
+
+//Output:
+//error: variable or field 'p' declared void
---
+++ SEMESTER5/PPWC/Assignment5/Question8.c
@@ -0,0 +1,8 @@
+//(1) int process(int (*pf)(int a, int b)) ;
+//(2) int (*fun(int, void (*ptr)()))();
+//(3) int *(*p)(int (*a)[ ]);
+//(4) int (*p)[10];
+//(5) float *p[20];
+//(6) int p(char *a);
+//(7) int (*p(char * a))[l0];
+//(8) int * (*p [10]) (char *a);
