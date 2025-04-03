051af199745e0efd757dc2b22e93b81f5a88a271
Pratehu <pragyanpriyadarshini107@gmail.com>
2025-04-03 10:46:54 +0530

    UPDATED

---
+++ SEMESTER5/PPWC/Assignment5/Question20.c
@@ -0,0 +1,11 @@
+/*Which of the following statements are true?.
+(1) (void *)0 is a void pointer
+(2) (void *)0 is a NULL pointer
+(3) int *p=(int *)0; p is a NULL pointer
+(4) a[i]==i[a]
+(5) a[i][j]== *(*(a+i)+j)
+
+
+
+ANSWER:
+ All statements are true (1, 2, 3, 4, and 5).
